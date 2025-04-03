792e8c5af6cc24c96b72c54b70c130f0c35a79d1
Meloooo88 <brosotoadiel48@gmail.com>
2025-04-03 21:57:03 +0800

    try

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/DefaultPage/Login";
+import Register from "./components/DefaultPage/Register";
+import Dashboard from "./components/DefaultPage/Dashboard";
+import Cart from "./components/CustomerPage/Cart"; // Make sure Cart.js exists and is correctly imported
+
+const isAuthenticated = () => !!localStorage.getItem("token");
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                {/* Public Routes */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                {/* Protected Route for Cart */}
+                <Route
+                    path="/cart"
+                    element={isAuthenticated() ? <Cart /> : <Navigate to="/login" />}
+                />
+
+                {/* Protected Route for Dashboard */}
+                <Route
+                    path="/dashboard"
+                    element={isAuthenticated() ? <Dashboard /> : <Navigate to="/login" />}
+                />
+
+                {/* Redirect unknown routes to login */}
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
